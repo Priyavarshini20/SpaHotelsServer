1f0f2376a9b45139db27fa5a79da3fa35e288cc3
A.P.Priyavarshini <priyavarshini383@gmail.com>
2025-01-29 14:57:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello from Wasan World");
+  res.send("SPA HOTELS SERVER IS RUNNING");
 });
 const roomsRoutes = require("./routes/roomRoutes");
 const userRoutes = require("./routes/userRoutes");
